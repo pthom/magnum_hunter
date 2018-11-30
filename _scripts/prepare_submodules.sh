6f951a4ca6fefbead77af66794bb0e8c7a775535
Pascal Thomet <pthomet@gmail.com>
2018-11-30 14:42:28 +0100

    prepare_submodules.sh : keep origin

--- _scripts/prepare_submodules.sh
+++ _scripts/prepare_submodules.sh
@@ -7,15 +7,11 @@ MAINREPODIR=$THISDIR/..
 
 cd $MAINREPODIR/magnum
 git remote add upstream git@github.com:mosra/magnum.git
-git remote add wip git@github.com:pthom/magnum.git
-git remote rm origin
-git fetch wip
-git checkout wip/magnum_hunter -b magnum_hunter
+git fetch origin
+git checkout origin/magnum_hunter -b magnum_hunter
 
 
 cd $MAINREPODIR/hunter
 git remote add upstream git@github.com:ruslo/hunter.git
-git remote add wip git@github.com:pthom/hunter.git
-git remote rm origin
-git fetch wip
-git checkout wip/test.magnum -b test.magnum
+git fetch origin
+git checkout origin/test.magnum -b test.magnum
