6856d17e8f344c9b9f942460422d959df256c2e3
Pascal Thomet <pthomet@gmail.com>
2018-11-30 20:45:24 +0100

    prepare_submodules.sh : do not remote
    (hub fails with several remotes...)

--- _scripts/prepare_submodules.sh
+++ _scripts/prepare_submodules.sh
@@ -6,12 +6,12 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 MAINREPODIR=$THISDIR/..
 
 cd $MAINREPODIR/magnum
-git remote add upstream git@github.com:mosra/magnum.git
+# git remote add upstream git@github.com:mosra/magnum.git # hub fails with several remotes ...
 git fetch origin
 git checkout origin/magnum_hunter -b magnum_hunter
 
 
 cd $MAINREPODIR/hunter
-git remote add upstream git@github.com:ruslo/hunter.git
+# git remote add upstream git@github.com:ruslo/hunter.git # hub fails with several remotes ...
 git fetch origin
 git checkout origin/test.magnum -b test.magnum
